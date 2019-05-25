d72b39618dbfedf00da7a82a0430731dbb6b6984
Kruzer97 <pokorski-marcin@wp.pl>
2019-05-25 23:39:36 +0200

    View did not work

---
+++ foodprovider/src/components/Contact.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View } from "react";
+
+
+const Contact = (props) => {
+    const { pStyle, viewStyle } = styles;
+    
+    return ( 
+    <div>
+        <View style={viewStyle}>
+        <p style={pStyle}>
+            
+            
+            Witam Państwa!
+
+        </p>
+        </View>
+    </div>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: "#F8F8F8"
+    },
+
+    pStyle:
+    {
+        fontSize: 20
+    }
+}
+
+export default Contact;
